3784a7ca0a24540599e7bae752e374e6cbcfa261
syaw0 <siamhb7@protonmail.com>
2022-05-11 02:51:39 +0430

    created a news list section and modified and responsived

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import {swiper} from "./modules/swiper"
 import {fetchdata} from "./modules/fetchdata"
 import {home} from "./modules/home"
 import {newsDetail} from "./modules/newsDetail"
-
+import {newsList} from "./modules/newslist"
 
 function insertElement(element){
     document.getElementById("main-page-con").innerHTML=`<h1>LOADING...</h1>`
@@ -13,7 +13,7 @@ function insertElement(element){
         document.getElementById("main-page-con").innerHTML=`${element}`
     },1000)
 }
-insertElement(newsDetail())
+insertElement(newsList())
 // fetchdata()
 scroll()
 swiper()
---
+++ src/modules/newslist.js
@@ -0,0 +1,351 @@
+export function newsList(){
+    return componnetData
+}
+
+
+const componnetData =`   <div id="newslist-con">
+<div id="newslist">
+  <div id="newslist-head">
+    <h3 id="newslist-head-header">Search For Results :</h3>
+    <div id="newslist-top-search-con">
+      <input type="search" name="search" title="search a news with a keywords an press enter o click on icon" id="newslist-top-search" class="searchbox header-top-search" placeholder="Search News Using Keywords..." />
+      <svg xmlns="http://www.w3.org/2000/svg" id="newslist-top-search-icon " class="header-top-search-icon" width="24" height="24" viewBox="0 0 24 24">
+        <path
+          fill-rule="evenodd"
+          d="M16.3198574,14.9056439 L21.7071068,20.2928932 L20.2928932,21.7071068 L14.9056439,16.3198574 C13.5509601,17.3729184 11.8487115,18 10,18 C5.581722,18 2,14.418278 2,10 C2,5.581722 5.581722,2 10,2 C14.418278,2 18,5.581722 18,10 C18,11.8487115 17.3729184,13.5509601 16.3198574,14.9056439 Z M10,16 C13.3137085,16 16,13.3137085 16,10 C16,6.6862915 13.3137085,4 10,4 C6.6862915,4 4,6.6862915 4,10 C4,13.3137085 6.6862915,16 10,16 Z"
+        ></path>
+      </svg>
+    </div>
+  </div>
+
+  <div id="newslist-body">
+    <div class="news-card-vertical">
+      <div class="news-card-vertical-img-con">
+        <img src="https://sportshub.cbsistatic.com/i/2022/05/07/16949311-976e-4131-a832-95f52b97cca4/jujutsu-kaisen.jpg" alt="" />
+      </div>
+      <div class="news-card-vertical-information">
+        <h6 class="side-news-without-img-category">
+          Sport
+        </h6>
+        <h4 class="news-card-vertical-information-title">
+          Jujutsu Kaisen/7-11 Collab Offers Wild New Art
+        </h4>
+        <h6 class="news-card-vertical-information-description">
+          Jujutsu Kaisen has become one of the biggest Shonen series that runs in the regularly released publication of Weekly Shonen Jump...
+        </h6>
+        <h6 class="news-card-vertical-information-date">
+          2022-05-07 20:59:00
+        </h6>
+      </div>
+    </div>
+    <div class="news-card-vertical">
+      <div class="news-card-vertical-img-con">
+        <img src="https://sportshub.cbsistatic.com/i/2022/05/07/16949311-976e-4131-a832-95f52b97cca4/jujutsu-kaisen.jpg" alt="" />
+      </div>
+      <div class="news-card-vertical-information">
+        <h6 class="side-news-without-img-category">
+          Sport
+        </h6>
+        <h4 class="news-card-vertical-information-title">
+          Jujutsu Kaisen/7-11 Collab Offers Wild New Art
+        </h4>
+        <h6 class="news-card-vertical-information-description">
+          Jujutsu Kaisen has become one of the biggest Shonen series that runs in the regularly released publication of Weekly Shonen Jump...
+        </h6>
+        <h6 class="news-card-vertical-information-date">
+          2022-05-07 20:59:00
+        </h6>
+      </div>
+    </div>
+    <div class="news-card-vertical">
+      <div class="news-card-vertical-img-con">
+        <img src="https://sportshub.cbsistatic.com/i/2022/05/07/16949311-976e-4131-a832-95f52b97cca4/jujutsu-kaisen.jpg" alt="" />
+      </div>
+      <div class="news-card-vertical-information">
+        <h6 class="side-news-without-img-category">
+          Sport
+        </h6>
+        <h4 class="news-card-vertical-information-title">
+          Jujutsu Kaisen/7-11 Collab Offers Wild New Art
+        </h4>
+        <h6 class="news-card-vertical-information-description">
+          Jujutsu Kaisen has become one of the biggest Shonen series that runs in the regularly released publication of Weekly Shonen Jump...
+        </h6>
+        <h6 class="news-card-vertical-information-date">
+          2022-05-07 20:59:00
+        </h6>
+      </div>
+    </div>
+    <div class="news-card-vertical">
+      <div class="news-card-vertical-img-con">
+        <img src="https://sportshub.cbsistatic.com/i/2022/05/07/16949311-976e-4131-a832-95f52b97cca4/jujutsu-kaisen.jpg" alt="" />
+      </div>
+      <div class="news-card-vertical-information">
+        <h6 class="side-news-without-img-category">
+          Sport
+        </h6>
+        <h4 class="news-card-vertical-information-title">
+          Jujutsu Kaisen/7-11 Collab Offers Wild New Art
+        </h4>
+        <h6 class="news-card-vertical-information-description">
+          Jujutsu Kaisen has become one of the biggest Shonen series that runs in the regularly released publication of Weekly Shonen Jump...
+        </h6>
+        <h6 class="news-card-vertical-information-date">
+          2022-05-07 20:59:00
+        </h6>
+      </div>
+    </div>
+    <div class="news-card-vertical">
+      <div class="news-card-vertical-img-con">
+        <img src="https://sportshub.cbsistatic.com/i/2022/05/07/16949311-976e-4131-a832-95f52b97cca4/jujutsu-kaisen.jpg" alt="" />
+      </div>
+      <div class="news-card-vertical-information">
+        <h6 class="side-news-without-img-category">
+          Sport
+        </h6>
+        <h4 class="news-card-vertical-information-title">
+          Jujutsu Kaisen/7-11 Collab Offers Wild New Art
+        </h4>
+        <h6 class="news-card-vertical-information-description">
+          Jujutsu Kaisen has become one of the biggest Shonen series that runs in the regularly released publication of Weekly Shonen Jump...
+        </h6>
+        <h6 class="news-card-vertical-information-date">
+          2022-05-07 20:59:00
+        </h6>
+      </div>
+    </div>
+    <div class="news-card-vertical">
+      <div class="news-card-vertical-img-con">
+        <img src="https://sportshub.cbsistatic.com/i/2022/05/07/16949311-976e-4131-a832-95f52b97cca4/jujutsu-kaisen.jpg" alt="" />
+      </div>
+      <div class="news-card-vertical-information">
+        <h6 class="side-news-without-img-category">
+          Sport
+        </h6>
+        <h4 class="news-card-vertical-information-title">
+          Jujutsu Kaisen/7-11 Collab Offers Wild New Art
+        </h4>
+        <h6 class="news-card-vertical-information-description">
+          Jujutsu Kaisen has become one of the biggest Shonen series that runs in the regularly released publication of Weekly Shonen Jump...
+        </h6>
+        <h6 class="news-card-vertical-information-date">
+          2022-05-07 20:59:00
+        </h6>
+      </div>
+    </div>
+  </div>
+
+  <div id="newslist-footer">
+    <button id="newslist-loadmore" class="btn-ghost">LOAD MORE</button>
+  </div>
+</div>
+
+<div id="newslist-nav" class="home-sections side-topnew">
+  <h3 id="newslist-nav-header">Top News</h3>
+
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+  <div class="side-news-without-img">
+    <h6 class="side-news-without-img-category">
+      Sport
+    </h6>
+    <h6 class="side-news-without-img-date">
+      2022-05-06 11:00:00
+    </h6>
+    <h4 class="side-news-without-img-title">
+      Big E provides major health update
+    </h4>
+  </div>
+
+  <!-- side news with img -->
+
+  <div class="side-news-with-img">
+    <div class="side-news-with-img-imgcon">
+      <img src="https://static.sportskeeda.com/editor/2022/05/05265-16518403424595-1920.jpg" alt="" />
+    </div>
+
+    <div class="side-news-with-img-infos">
+      <h6 class="side-news-with-img-infos-date">
+        2022-05-06 11:00:00
+      </h6>
+
+      <h4 class="side-news-with-img-infos-title">
+        Man dies in Talladega shooting, suspect sought
+      </h4>
+    </div>
+  </div>
+  <div class="side-news-with-img">
+    <div class="side-news-with-img-imgcon">
+      <img src="https://static.sportskeeda.com/editor/2022/05/05265-16518403424595-1920.jpg" alt="" />
+    </div>
+
+    <div class="side-news-with-img-infos">
+      <h6 class="side-news-with-img-infos-date">
+        2022-05-06 11:00:00
+      </h6>
+
+      <h4 class="side-news-with-img-infos-title">
+        Man dies in Talladega shooting, suspect sought
+      </h4>
+    </div>
+  </div>
+  <div class="side-news-with-img">
+    <div class="side-news-with-img-imgcon">
+      <img src="https://static.sportskeeda.com/editor/2022/05/05265-16518403424595-1920.jpg" alt="" />
+    </div>
+
+    <div class="side-news-with-img-infos">
+      <h6 class="side-news-with-img-infos-date">
+        2022-05-06 11:00:00
+      </h6>
+
+      <h4 class="side-news-with-img-infos-title">
+        Man dies in Talladega shooting, suspect sought
+      </h4>
+    </div>
+  </div>
+  <div class="side-news-with-img">
+    <div class="side-news-with-img-imgcon">
+      <img src="https://static.sportskeeda.com/editor/2022/05/05265-16518403424595-1920.jpg" alt="" />
+    </div>
+
+    <div class="side-news-with-img-infos">
+      <h6 class="side-news-with-img-infos-date">
+        2022-05-06 11:00:00
+      </h6>
+
+      <h4 class="side-news-with-img-infos-title">
+        Man dies in Talladega shooting, suspect sought
+      </h4>
+    </div>
+  </div>
+  <div class="side-news-with-img">
+    <div class="side-news-with-img-imgcon">
+      <img src="https://static.sportskeeda.com/editor/2022/05/05265-16518403424595-1920.jpg" alt="" />
+    </div>
+
+    <div class="side-news-with-img-infos">
+      <h6 class="side-news-with-img-infos-date">
+        2022-05-06 11:00:00
+      </h6>
+
+      <h4 class="side-news-with-img-infos-title">
+        Man dies in Talladega shooting, suspect sought
+      </h4>
+    </div>
+  </div>
+  <div class="side-news-with-img">
+    <div class="side-news-with-img-imgcon">
+      <img src="https://static.sportskeeda.com/editor/2022/05/05265-16518403424595-1920.jpg" alt="" />
+    </div>
+
+    <div class="side-news-with-img-infos">
+      <h6 class="side-news-with-img-infos-date">
+        2022-05-06 11:00:00
+      </h6>
+
+      <h4 class="side-news-with-img-infos-title">
+        Man dies in Talladega shooting, suspect sought
+      </h4>
+    </div>
+  </div>
+</div>
+</div>
+`
